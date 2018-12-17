84cc44caaab99faeedb5d634c124e876dea87243
Jimmy Stone <jstone05@hamline.edu>
2018-12-16 20:56:41 -0600

    updates

---
+++ assets/app.js
@@ -0,0 +1,45 @@
+var q1 = {
+    question:"Choose one",
+    answer: "1",
+    options: ["1,2,3,4"],
+    rightAnswer: 1
+};
+
+var q2 = {
+    question:"Choose Two",
+    answer: "2",
+    options: ["1,2,3,4"],
+    rightAnswer:2
+};
+
+var q3 = {
+    question:"Choose Three",
+    answer: "3",
+    options: ["1,2,3,4"],
+    rightAnswer:3
+};
+
+var q4 = {
+    question:"Choose Four",
+    answer: "4",
+    options: ["1,2,3,4"],
+    rightAnswer:4
+};
+
+// var q5 = {
+//     question:"",
+//     answer: "",
+//     options: [""],
+//     rightAnswer:1
+// };
+
+// var q6 = {
+//     question:"",
+//     answer: "",
+//     options: [""],
+//     rightAnswer:1
+// };
+
+var qArray = [q1,q2,q3,q4];
+
+var qArraryIndex;
